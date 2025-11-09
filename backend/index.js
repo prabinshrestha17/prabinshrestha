aea42cd2419962a2376ecaaf6af907768d143d59
prabinshrestha <Prabin70>
2025-11-09 14:41:51 +0545

    url updated

--- backend/index.js
+++ backend/index.js
@@ -3,44 +3,28 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const contactRouter = require("./src/routes/user.routes");
 const connectDB = require("./src/database/connectDB");
-
-const PROD_ORIGIN = process.env.BASEURL;
+const { allowedUrl } = require("./src/api/dotenv");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
-
-if (PROD_ORIGIN) {
-  allowedOrigins.push(PROD_ORIGIN);
-}
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
-
+app.use(
+  cors(
+    origin({
+      origin: allowedUrl,
+      methods: ["GET", "POST"],
+    })
+  )
+);
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World! API is running.");
+  res.send("Hello World!");
 });
 
 app.use("/contact", contactRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
