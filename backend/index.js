db81b1bc4a85b967d40e92706deb1ccbecf7dfa6
prabinshrestha <Prabin70>
2025-11-09 15:26:00 +0545

    testing completed

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const contactRouter = require("./src/routes/user.routes");
 const connectDB = require("./src/database/connectDB");
+const { frontendUrl } = require("./src/api/dotenv");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,7 +13,7 @@ connectDB();
 // Allow requests only from your frontend
 app.use(
   cors({
-    origin: "https://prabinshrestha-theta.vercel.app", // replace with your frontend URL
+    origin: frontendUrl, // replace with your frontend URL
     credentials: true, // if you plan to use cookies or authentication
   })
 );
