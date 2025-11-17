346a1ac27b36b4da748031110cef94b3a104c128
prabinshrestha <Prabin70>
2025-11-17 21:15:14 +0545

    feat: url updated

--- client/src/api/env.ts
+++ client/src/api/env.ts
@@ -1,3 +1,4 @@
-const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const baseUrl =
+  process.env.NEXT_PUBLIC_API_URL || "https://prabinshrestha-2154.vercel.app";
 
 export { baseUrl };
